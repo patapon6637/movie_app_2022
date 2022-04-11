c4c06c3543a786c8e7407a46ecb23e528bfd97db
patapon6637 <wh00358@gmail.com>
2022-04-11 12:18:34 +0900

    리액트 무비 앱 업로드

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Navigation/>
       <Routes>
         <Route path='/' element={<Home />} />
